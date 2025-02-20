f54b6ddb3ba159910f34acb6ea2a7032a68e2b73
bonzo4 <alonzo.sabino814@gmail.com>
2025-02-19 18:39:50 -0600

    feat: npm package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Elysia } from "elysia";
 import { authRouter } from "./route/auth";
 
-const app = new Elysia().use(authRouter).listen(process.env.PORT!);
+const gatewayApi = new Elysia().use(authRouter).listen(process.env.PORT!);
+
+type GatewayApi = typeof gatewayApi;
+export type { GatewayApi };
